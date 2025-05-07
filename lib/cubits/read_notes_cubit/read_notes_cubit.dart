2ef1ca54a48d6f7ca7e902009ee0eaec1eea8bf8
ahmedhussien12 <ahmed.hussien950.ah@gmail.com>
2025-05-07 03:02:40 +0300

    finish read notes cubit create the bloc provider for read notes cubit

--- lib/cubits/read_notes_cubit/read_notes_cubit.dart
+++ lib/cubits/read_notes_cubit/read_notes_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 
+import '../../constants/constants.dart';
 import '../../model/note_model.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    try {
+      List<NoteModel> notes = notesBox.values.toList(); // List<NoteModel>
+      emit(ReadNotesSuccess(notes));
+    } catch (e) {
+      emit(ReadNotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/read_notes_cubit/read_notes_cubit.dart';
 
 import '../widgets/add_new_note.dart';
 import '../widgets/notes_view_body.dart';
@@ -10,24 +12,31 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNewNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add, color: Colors.black),
+    return BlocProvider(
+      // hena bs m3loma hal hyfr2 lw 3mlt create hena aw
+      // aw 3la el material app f el 7ala d msh hyfr2
+      // 3shan el el notes view m3aia w sh8ala tol ma ana fat7
+      // el app w msh htmoot l2nha hya el app asln
+      create: (context) => ReadNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNewNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add, color: Colors.black),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
